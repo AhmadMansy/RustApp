7fa18882dc8f85826db31982618f7eb911e4f41a
Ahmed Mansy <54113248+AhmadMansy@users.noreply.github.com>
2023-08-27 00:17:02 +0300

    first guess game

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,34 @@
-use std::io::{stdout, Stdout};
+use std::cmp::Ordering;
+use std::io::{stdin};
+use rand::Rng;
+use colored;
+use colored::Colorize;
 
-fn main() -> Stdout {
-    println!("Hello, world!");
-    let stdout = stdout();
+fn main() {
+    guess_game();
+}
 
+fn guess_game(){
+    let rand:i32 = rand::thread_rng().gen_range( 1, 101);
+    let mut count = 0;
+    loop {
+        let mut guess = String::new();
+        println!("Please enter your guess");
+        stdin().read_line(&mut guess)
+            .expect("failed to read line");
+        count += 1;
+        let guess: i32 = match guess.trim().parse() {
+            Ok(num)=> num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&rand){
+            Ordering::Less => { println!("{}","Too small!".red())}
+            Ordering::Equal => {
+                println!("{}",format!("you win in [{}] attempts", count).green());
+                break;
+            }
+            Ordering::Greater => {println!("{}", "Too big!".red())}
+        }
+    }
 }
